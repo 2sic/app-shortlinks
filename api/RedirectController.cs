b6df4ea04ed6c78100a6919170b80e57e547cd39
iJungleboy <daniel.mettler@2sic.com>
2017-03-15 13:58:23 +0100

    creating api - almost ready...

--- api/RedirectController.cs
+++ api/RedirectController.cs
@@ -11,14 +11,43 @@ using System.Runtime.CompilerServices;
 using DotNetNuke.Services.Mail;
 using Newtonsoft.Json;
 
-public class FormController : SxcApiController
+public class RedirectController : SxcApiController
 {
     [AllowAnonymous] 
 	[HttpGet]
     [DnnModuleAuthorize(AccessLevel = SecurityAccessLevel.Anonymous)]
-    public void Go([FromUrl]string key)
+    public string Go(string key, string domain = "", string full = null)
     {
+        // lower the key, just to be sure
+        key = key.ToLower();
 
+        // 1. get all links
+        var all = AsDynamic(App.Data["Link"]);
+
+        // 2. try to find the key all lower case (assumes that the system is case-insensitive)
+        var current = all.FirstOrDefault(l => l.Key == key);
+
+        if(current == null)
+            return GoToDefault(key);
+
+        // todo: 
+        // 4. if yes and not retired: redirect
+        // 5. if yes and retired: redirect to app-setting
+        if(current.Retired != null && current.Retired)
+            return GoToDefault(key);
+
+        // todo: maybe log?
+
+        // redirect
+        Response.Redirect(current.Link);
+    }
+
+    private string GoToDefault(string key) {
+        var fallback = App.Settings.FallbackLink;
+        if(fallback == null)
+            throw new Exception("can't find real link and fallback fails too");
+
+        return fallback;
     }
 
 
